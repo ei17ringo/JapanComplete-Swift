3ef98a39f81e51fe829c32d34c88f06dfc8df33e
ei17ringo <ei17ringo@gmail.com>
2016-12-31 00:24:59 +0800

    Map部分完成（シェア機能除く）

--- JapanComplete/ViewController.swift
+++ JapanComplete/ViewController.swift
@@ -18,6 +18,13 @@ class ViewController: UIViewController,UIWebViewDelegate,GADBannerViewDelegate {
     
     @IBOutlet weak var shareButton: UIButton!
     
+    @IBOutlet weak var percentageText: UILabel!
+    
+    var colorArea:NSMutableDictionary = NSMutableDictionary()
+    
+
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -27,13 +34,27 @@ class ViewController: UIViewController,UIWebViewDelegate,GADBannerViewDelegate {
         //画面上部のデザイン設定
         setUpperBar()
         
+        //初期表示はUserDefaultに値を作成
+        var areaDefault = UserDefaults.standard
+        var tmp:NSMutableDictionary! = areaDefault.object(forKey: "colorArea") as! NSMutableDictionary!
+        if tmp != nil {
+            colorArea = tmp!.mutableCopy() as! NSMutableDictionary
+            print("success to get data from UserDefault")
+            calcPercentage()
+            
+        }else{
+            colorArea = setAreaDictionary("-1",areaID: "")
+        }
+        
+        print(colorArea)
+        
+        //map表示
+        viewMap()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        
-        //map表示
-        viewMap()
+       
     }
 
     // map表示
@@ -65,6 +86,128 @@ class ViewController: UIViewController,UIWebViewDelegate,GADBannerViewDelegate {
         helpButton.setFAIcon(icon: FAType.FAQuestionCircleO, forState: .normal)
         shareButton.setFAIcon(icon: FAType.FAShareSquareO, forState: .normal)
     }
+    
+    //UserDefaultに保存している値を地図に反映する
+    func webViewDidFinishLoad(_ webView: UIWebView) {
+        
+        if colorArea.count == 0 {
+            var areaDefault = UserDefaults.standard
+            var tmp:NSMutableDictionary! = areaDefault.dictionary(forKey: "colorArea") as! NSMutableDictionary!
+            colorArea = tmp
+
+        }
+        
+        var keys:NSArray = colorArea.allKeys as NSArray
+        
+        for i in 0 ..< (keys.count) {
+            var key:String = keys[i] as! String
+            var value:String = String(describing: colorArea.object(forKey: key)!)
+            
+            var command:String = "setcolor('\(value)','\(key)');"
+            
+            mapWebView.stringByEvaluatingJavaScript(from: command)
+        }
+        
+
+    }
+    
+    //javascriptから値を受け取りUserDefaultに設定する
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        print(request.url)
+        
+        var geturl:URL = request.url!
+        
+        if geturl.scheme == "webview" {
+            
+            let comp: NSURLComponents? = NSURLComponents(string: geturl.absoluteString)
+            
+            var colorCode = "0"
+            var areaID = ""
+            for i in 0 ..< (comp?.queryItems?.count)! {
+                let item = (comp?.queryItems?[i])! as URLQueryItem
+                print("name=\(item.name), value=\(item.value)")
+                
+                switch item.name {
+                case "color":
+                    colorCode = item.value!
+                    break
+                case "id":
+                    areaID = item.value!
+                    break
+                default:
+                    print("unknownname=\(item.name), value=\(item.value)")
+                }
+            }
+
+            setAreaDictionary(colorCode,areaID: areaID)
+            
+        }
+        
+        return true
+    }
+    
+    //Area色ディクショナリー設定
+    func setAreaDictionary(_ colorCode:String, areaID:String) -> NSMutableDictionary{
+        
+        var areaDefault = UserDefaults.standard
+
+        //初期設定（全て白）
+        if colorCode == "-1" {
+            
+            //json.txtファイルを読み込んで
+            let path = Bundle.main.path(forResource: "prefecture", ofType: "json")
+            
+            let jsondata = try? Data(contentsOf: URL(fileURLWithPath: path!))
+            
+            //配列データに変換して
+            let jsonArray = (try! JSONSerialization.jsonObject(with: jsondata!, options: [])) as! NSArray
+            
+            //配列の個数だけUserDefaultに保存
+            
+            for dat in jsonArray{
+                var areaTmpDic = dat as! NSDictionary
+                colorArea.setValuesForKeys([areaTmpDic["name"] as! String : areaTmpDic["code"]])
+
+            }
+
+
+        }else{
+            print(colorArea)
+            colorArea.setValuesForKeys([areaID : colorCode])
+        }
+        
+        print(colorArea)
+        areaDefault.set(colorArea, forKey: "colorArea")
+        areaDefault.synchronize()
+
+        calcPercentage()
+        
+        return colorArea
+    }
+    
+    //パーセント計算・表示
+    func calcPercentage(){
+    
+        var keys:NSArray = colorArea.allKeys as NSArray
+        var countColored = 0
+        
+        for i in 0 ..< (keys.count) {
+            var key:String = keys[i] as! String
+            var value:String = String(describing: colorArea.object(forKey: key)!)
+            
+            if Int(value)! > 0 {
+                countColored += 1
+            }
+        }
+        
+        let perfectNumber:Float = 47.00
+        var percentage:Float = Float(countColored) / perfectNumber * 100.00
+        
+        percentageText.text = "Your Score : \(percentage)% \(countColored) / 47 areas"
+
+    }
+    
     
     // 広告表示
     func viewAdmob(){
